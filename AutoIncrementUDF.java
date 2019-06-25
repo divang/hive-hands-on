e4df763d5c66996247295ca94abc4689ea997dfa
Divang <divang.s@gmail.com>
2019-06-25 20:37:03 +0530

    Update AutoIncrementUDF.java

--- AutoIncrementUDF.java
+++ AutoIncrementUDF.java
@@ -3,8 +3,8 @@ import org.apache.hadoop.hive.ql.udf.UDFType;
 /*
  * Here make sure that addition of annotation @UDFType(stateful = true) is required otherwise counter value will not get increment in the Hive column, it will just return value 1 for all the rows but not the actual row number.
  */
-@UDFType(stateful = true)
-public class AutoIncrementUDF extends UDF 
+@UDFType(stateful = true, 	deterministic=false)
+public class AutoIncrementUDF extends UDF
 {
 int ctr;
 public int evaluate() {
